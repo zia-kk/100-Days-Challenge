8c8ec9be3dc5dea4d544ee738e1b43529f77d925
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-18 19:39:16 +0500

    Day#3 Challenge

---
+++ Day#3/Q8/q8.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 5;
+let num1 = 3;
+console.log(num + num1);
---
+++ Day#3/Q8/q8.ts
@@ -0,0 +1,6 @@
+// let num = 5;
+// let num1 = 3;
+
+// console.log(num + num1);
+
+console.log(5 + 3);
