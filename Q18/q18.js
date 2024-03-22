2ca9cde41cd801b4b83463a9c013220a1348f037
Zia Ur Rehman <162309768+zia-kk@users.noreply.github.com>
2024-03-22 17:23:12 +0500

    Delete Q18 directory

--- Q18/q18.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let Places = ["karachi", "Faisalabd", "Lahore", "Multan", "Islambad", "Hyderabad"];
-//  console.log("orignal " + Places);
-//  console.log("copy " + [...Places].sort());
-console.log("copy " + [...Places].sort().reverse());
-console.log("copy " + [...Places].sort().reverse());
-console.log("copy " + [...Places].sort());
-console.log("copy " + [...Places].sort().reverse());
