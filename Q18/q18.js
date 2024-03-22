d09a803dd2d5bba338b9a6f622c63258cb76eab2
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 17:21:15 +0500

    update

---
+++ Q18/q18.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Places = ["karachi", "Faisalabd", "Lahore", "Multan", "Islambad", "Hyderabad"];
+//  console.log("orignal " + Places);
+//  console.log("copy " + [...Places].sort());
+console.log("copy " + [...Places].sort().reverse());
+console.log("copy " + [...Places].sort().reverse());
+console.log("copy " + [...Places].sort());
+console.log("copy " + [...Places].sort().reverse());
---
+++ Q18/q18.ts
@@ -0,0 +1,18 @@
+let Places: string []=["karachi","Faisalabd","Lahore","Multan","Islambad","Hyderabad"]
+//  console.log("orignal " + Places);
+
+//  console.log("copy " + [...Places].sort());
+
+ console.log("copy " + [...Places].sort().reverse());
+
+ console.log("copy " + [...Places].sort().reverse());
+
+ console.log("copy " + [...Places].sort());
+
+ console.log("copy " + [...Places].sort().reverse());
+
+
+
+
+ 
+ 
